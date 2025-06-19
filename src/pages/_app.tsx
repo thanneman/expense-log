e073280350190aeb88d10144c929c5cfa3d613c8
Travis Hanneman <thanneman1@gmail.com>
2025-06-18 20:08:02 -0700

    chore: project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Geist } from 'next/font/google'
+
+const geist = Geist({ variable: '--font-geist-sans', subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+  return (
+    <div className={geist.className}>
+      <Component {...pageProps} />
+    </div>
+  )
+}   
